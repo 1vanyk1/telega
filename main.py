b8a201942d4a230df5a92c0821ebc393b1c8bb73
1vanyk1 <57059188+1vanyk1@users.noreply.github.com>
2020-04-01 20:51:03 +0300

    1

--- main.py
+++ main.py
@@ -1,72 +1,57 @@
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import json
-import random
+import requests
 import copy
-reply_keyboard = [['/yes', '/no']]
+reply_keyboard = [['/lang ru en', '/lang en ru']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-current_question = 0
-questions = json.loads('test.json')["test"]
-test = []
-ans = [0, 0]
+REQUEST_KWARGS = {'proxy_url': 'socks5://173.245.239.12:17145',
+                  'urllib3_proxy_kwargs': {'assert_hostname': 'False',
+                                           'cert_reqs': 'CERT_NONE'} }
 
 
-def generate_test():
-    global test, current_question
-    if len(questions) > 10:
-        test = random.sample(questions, 10)
-    test = copy.copy(questions)
-    random.shuffle(test)
-    ans.clear()
-    ans.append(0)
-    ans.append(0)
-    current_question = 0
-
-
-def echo(update, context):
+def echo(update, context, user_data):
     update.message.reply_text(f"Я получил сообщение {update.message.text}")
 
 
-def start(update, context):
-    generate_test()
-    update.message.reply_text("Здраствуйте! Не хотите пройти опрос?", reply_markup=markup)
+def start(update, context, user_data):
+    context.user_data['lang'] = 'ru-en'
+    update.message.reply_text("Здраствуйте, я бот-переводчик. Я могу переводить с русского на английский и наоборот. Просто напишите слово, которое нужно перевести.", reply_markup=markup)
     return 1
 
 
-def start_test(update, context):
-    update.message.reply_text(questions[0]['question'])
-    return 2
+def change_language(update, context, user_data):
+    context.user_data['lang'] = f'{context.args[0]}-{context.args[1]}'
 
 
-def next_questions(update, context):
-    global current_question
-    answer = update.message.text
-    if answer == questions[current_question]['response']:
-        ans[0] = ans[0] + 1
-    else:
-        ans[1] = ans[1] + 1
-    current_question += 1
-    if current_question == len(questions):
-        update.message.reply_text(f'''Поздравляем! Вы окончили тест! Вы ответили правильно на 
-        {ans[0]} вопросов и неправильно на {ans[1]} вопросов''')
-        update.message.reply_text('Хотите ещё раз пройти тест?', reply_markup=markup)
-        return 1
-    else:
-        update.message.reply_text(questions[current_question]['question'])
-    return 2
-
-
-def stop(update, context):
+def stop(update, context, user_data):
     return ConversationHandler.END
 
 
+def translate(update, context, user_data):
+    context.user_data['locality'] = update.message.text
+    api_server = "https://translate.yandex.net/api/v1.5/tr.json/translate"
+    params = {
+        "key":
+            'trnsl.1.1.20200325T150356Z.9f78a6ddcc4f1fc4.dc324d7ca5564e2f768570df2a4a51690c20377d',
+        "text": update.message.text, "lang": context.user_data["lang"]}
+    request = requests.get(api_server, params=params)
+    try:
+        text = str(request.json()['text'])
+        update.message.reply_text(text)
+    except BaseException:
+        update.message.reply_text('Не удалось перевести: ' + update.message.text)
+    return 1
+
+
 def main():
-    updater = Updater('1022899407:AAGUFsXx6G4srC2T2mwvajLQHuQeJnJG5mU', use_context=True)
+    updater = Updater('1022899407:AAGUFsXx6G4srC2T2mwvajLQHuQeJnJG5mU', use_context=True, request_kwargs=REQUEST_KWARGS)
     dp = updater.dispatcher
-    conv_handler = ConversationHandler(entry_points=[CommandHandler('start', start)],
-                                       states={1: [CommandHandler('yes', start_test),
-                                                   CommandHandler('no', stop)],
-                                               2: [MessageHandler(Filters.text, next_questions)]},
+    dp.add_handler(CommandHandler("lang", change_language))
+    conv_handler = ConversationHandler(entry_points=[CommandHandler('start', start,
+                                                                    pass_user_data=True)],
+                                       states={1: [MessageHandler(Filters.text, translate,
+                                                                  pass_user_data=True)]},
                                        fallbacks=[CommandHandler('stop', stop)])
     dp.add_handler(conv_handler)
     updater.start_polling()
