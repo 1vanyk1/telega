911c2d009b8fdd94709e7d37d827fc417c9b59a2
1vanyk1 <57059188+1vanyk1@users.noreply.github.com>
2020-04-01 19:39:15 +0300

    1

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-reply_keyboard1 = [['/room 2', '/exit']]
-markup1 = ReplyKeyboardMarkup(reply_keyboard1, one_time_keyboard=False)
-reply_keyboard2 = [['/room 3']]
-markup2 = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=False)
-reply_keyboard3 = [['/room 4', '/room 1']]
-markup3 = ReplyKeyboardMarkup(reply_keyboard3, one_time_keyboard=False)
-reply_keyboard4 = [['/room 1']]
-markup4 = ReplyKeyboardMarkup(reply_keyboard4, one_time_keyboard=False)
-rooms_markups = [markup1, markup2, markup3, markup4]
-rooms_info = ['В данном зале представлены статуи римских воинов. Римляни очень ухотели показать свою силу, поэтому воины и война имело одно из важнейших мест в древнеримской культуре.',
-              'В данном зале представлены макеты католических церквей. Они выполнены в характерном для Средневековья готическом стиле.',
-              'В данном зале представлены картины Леонардо да Винчи и его чертежи. Он был великим учёным, опередившим своё время и жившим в период Возрождения.',
-              'В данном зале представлены картины 19 века. На них изображено стремление и желание народа освободиться от влиянии Австрии и Франции и объединиться в одно государство.']
+reply_keyboard = [['/yes', '/no']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+current_line = 0
+lines = ['Ты видел деву на скале', 'В одежде белой над волнами', 'Когда, бушуя в бурной мгле,',
+         'Играло море с берегами,', 'Когда луч молний озарял', 'Ее всечасно блеском алым',
+         'И ветер бился и летал', 'С ее летучим покрывалом?', 'Прекрасно море в бурной мгле',
+         'И небо в блесках без лазури;', 'Но верь мне: дева на скале',
+         'Прекрасней волн, небес и бури.']
 
 
 def echo(update, context):
@@ -20,32 +15,39 @@ def echo(update, context):
 
 
 def start(update, context):
-    update.message.reply_text("Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!")
-    update.message.reply_text(rooms_info[0], reply_markup=rooms_markups[0])
+    global current_line
+    update.message.reply_text(lines[0], reply_markup=ReplyKeyboardRemove())
+    current_line += 1
+    return 1
+
+
+def next_line(update, context):
+    global current_line
+    answer = update.message.text
+    if answer != lines[current_line]:
+        update.message.reply_text("нет, не так")
+        update.message.reply_text(f"Правильно: {lines[current_line]}. Попробуй повторить.")
+        return 1
+    if current_line + 1 == len(lines):
+        update.message.reply_text("Мы молодцы! Давай повторим!", reply_markup=markup)
+        return 2
+    update.message.reply_text(lines[current_line + 1])
+    current_line += 2
     return 1
 
 
 def stop(update, context):
-    update.message.reply_text("Всего доброго, не забудьте забрать верхнюю одежду в гардеробе!",
-                              reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
-
-
-def enter_room(update, context):
-    room = int(context.args[0])
-    update.message.reply_text(rooms_info[room - 1], reply_markup=rooms_markups[room - 1])
-    return room
 
 
 def main():
     updater = Updater('1022899407:AAGUFsXx6G4srC2T2mwvajLQHuQeJnJG5mU', use_context=True)
     dp = updater.dispatcher
     conv_handler = ConversationHandler(entry_points=[CommandHandler('start', start)],
-                                       states={1: [CommandHandler('room', enter_room)],
-                                               2: [CommandHandler('room', enter_room)],
-                                               3: [CommandHandler('room', enter_room)],
-                                               4: [CommandHandler('room', enter_room)]},
-                                       fallbacks=[CommandHandler('exit', stop)])
+                                       states={1: [MessageHandler(Filters.text, next_line)],
+                                               2: [CommandHandler('yes', start),
+                                                   CommandHandler('no', stop)]},
+                                       fallbacks=[CommandHandler('stop', stop)])
     dp.add_handler(conv_handler)
     updater.start_polling()
     updater.idle()
