7cc46e69ef92120a828b6f8d2efb60825c1e64c5
1vanyk1 <57059188+1vanyk1@users.noreply.github.com>
2020-04-01 20:27:12 +0300

    1

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+import json
+import random
+import copy
 reply_keyboard = [['/yes', '/no']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-current_line = 0
-lines = ['Ты видел деву на скале', 'В одежде белой над волнами', 'Когда, бушуя в бурной мгле,',
-         'Играло море с берегами,', 'Когда луч молний озарял', 'Ее всечасно блеском алым',
-         'И ветер бился и летал', 'С ее летучим покрывалом?', 'Прекрасно море в бурной мгле',
-         'И небо в блесках без лазури;', 'Но верь мне: дева на скале',
-         'Прекрасней волн, небес и бури.']
+current_question = 0
+questions = json.loads('test.json')["test"]
+test = []
+ans = [0, 0]
+
+
+def generate_test():
+    global test, current_question
+    if len(questions) > 10:
+        test = random.sample(questions, 10)
+    test = copy.copy(questions)
+    random.shuffle(test)
+    ans.clear()
+    ans.append(0)
+    ans.append(0)
+    current_question = 0
 
 
 def echo(update, context):
@@ -15,25 +28,32 @@ def echo(update, context):
 
 
 def start(update, context):
-    global current_line
-    update.message.reply_text(lines[0], reply_markup=ReplyKeyboardRemove())
-    current_line += 1
+    generate_test()
+    update.message.reply_text("Здраствуйте! Не хотите пройти опрос?", reply_markup=markup)
     return 1
 
 
-def next_line(update, context):
-    global current_line
+def start_test(update, context):
+    update.message.reply_text(questions[0]['question'])
+    return 2
+
+
+def next_questions(update, context):
+    global current_question
     answer = update.message.text
-    if answer != lines[current_line]:
-        update.message.reply_text("нет, не так")
-        update.message.reply_text(f"Правильно: {lines[current_line]}. Попробуй повторить.")
+    if answer == questions[current_question]['response']:
+        ans[0] = ans[0] + 1
+    else:
+        ans[1] = ans[1] + 1
+    current_question += 1
+    if current_question == len(questions):
+        update.message.reply_text(f'''Поздравляем! Вы окончили тест! Вы ответили правильно на 
+        {ans[0]} вопросов и неправильно на {ans[1]} вопросов''')
+        update.message.reply_text('Хотите ещё раз пройти тест?', reply_markup=markup)
         return 1
-    if current_line + 1 == len(lines):
-        update.message.reply_text("Мы молодцы! Давай повторим!", reply_markup=markup)
-        return 2
-    update.message.reply_text(lines[current_line + 1])
-    current_line += 2
-    return 1
+    else:
+        update.message.reply_text(questions[current_question]['question'])
+    return 2
 
 
 def stop(update, context):
@@ -44,9 +64,9 @@ def main():
     updater = Updater('1022899407:AAGUFsXx6G4srC2T2mwvajLQHuQeJnJG5mU', use_context=True)
     dp = updater.dispatcher
     conv_handler = ConversationHandler(entry_points=[CommandHandler('start', start)],
-                                       states={1: [MessageHandler(Filters.text, next_line)],
-                                               2: [CommandHandler('yes', start),
-                                                   CommandHandler('no', stop)]},
+                                       states={1: [CommandHandler('yes', start_test),
+                                                   CommandHandler('no', stop)],
+                                               2: [MessageHandler(Filters.text, next_questions)]},
                                        fallbacks=[CommandHandler('stop', stop)])
     dp.add_handler(conv_handler)
     updater.start_polling()
